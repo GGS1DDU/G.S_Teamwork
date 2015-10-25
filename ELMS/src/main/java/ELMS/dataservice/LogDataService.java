8c80e8d9246f0d465c663ca2cfd97362577760f0
Zhhittta <zhanght1111@outlook.com>
2015-10-25 22:47:02 +0800

    Change

--- ELMS/src/main/java/ELMS/dataservice/LogDataService.java
+++ ELMS/src/main/java/ELMS/dataservice/LogDataService.java
@@ -8,7 +8,7 @@ import ELMS.po.LogPO;
 
 public interface LogDataService {
 	public LogPO find(String id) throws RemoteException;
-	public ArrayList<LogPO> findall() throws RemoteException;
+	public ArrayList<LogPO> findAll() throws RemoteException;
 	public ArrayList<LogPO> findbyID(String ID)throws RemoteException;
 	public void insert(LogPO po) throws RemoteException;
 	public void init() throws RemoteException;
